8f13e43870f974eab747a39280d51739edb98fa7
hasiburrahman123 <hasibur@2018.cqut.edu.cn>
2021-04-14 16:43:49 +0800

    Initial commit to a java class

---
+++ src/javaClass.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: javaClass
+ * @Description: TODO
+ * @Author: 哈西
+ * @Date: 2021/4/14 16:43
+ * @Version: v1.0
+ */
+public class javaClass {
+    public static void main(String[] args) {
+        System.out.println("New java class");
+    }
+}
